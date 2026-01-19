e42cf2334b8875b2e01a4ed595887095d8255f3a
Robertino Mihai Sandorhazi <49100220+nightdevil00@users.noreply.github.com>
2026-01-19 23:08:27 +0200

    Add additional font packages to PACMAN_BASE_PKGS

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 PACMAN_BASE_PKGS=(
     git base-devel curl wget unzip rsync
-    wayland
+    wayland ttf-nerd-fonts-symbols ttf-cascadia-mono-nerd ttf-nerd-fonts-symbols-mono
     pipewire pipewire-pulse wireplumber pavucontrol
     polkit polkit-gnome
     xdg-user-dirs xdg-utils
